c9548b7883ec7a0c25fdd96368d20a6594994472
dilsh0d <dilshod.toj@gmail.com>
2020-10-07 21:52:12 +0500

    HelloWorld class

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package helloworld;
+
+/**
+ * @author: Dilsh0d Tadjiev on 07.10.2020 21:46.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Конселга оддий матн тапи чикади
+    }
+}
