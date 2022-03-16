82be91a3f0eb78f77053a178845b151f2515fb82
Dilshod Tadjiev <dilshod.toj@gmail.com>
2022-03-16 22:35:36 +0500

    This is example show Imperative and Declarative programming language style difference

---
+++ src/uz/dilshod/code/imperatevsdeclarative/ImperativeVsDeclarativeProgrammingLanguage.java
@@ -0,0 +1,33 @@
+package uz.dilshod.code.imperatevsdeclarative;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+/**
+ * This is example show Imperative and Declarative programming language style difference
+ */
+public class ImperativeVsDeclarativeProgrammingLanguage {
+
+    /**
+     * This is example prints sum of numbers [1,2,..,100]
+     */
+    public static void main(String[] args) {
+        /**
+         * Imperative style of programming
+         */
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+        }
+        System.out.println("Imperative style result at the Java: " + sum);
+
+        /**
+         * Declarative style of programming
+         */
+        int sum1 = 0;
+        sum1 = IntStream
+                .rangeClosed(1,100)
+                .sum();
+        System.out.println("Declarative style  result at the Java: " + sum1);
+    }
+}
---
+++ src/uz/dilshod/code/imperatevsdeclarative/ImperativeVsDeclarativeProgrammingLanguage2.java
@@ -0,0 +1,39 @@
+package uz.dilshod.code.imperatevsdeclarative;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * This is example show Imperative and Declarative programming language style difference
+ */
+public class ImperativeVsDeclarativeProgrammingLanguage2 {
+    /**
+     * This is example print unique numbers from List
+     */
+    public static void main(String[] args) {
+        List<Integer> numberList = Arrays.asList(1,1,2,3,3,3,4,4,5,6,6,7,7,8,9,10,10,11);
+
+        /**
+         * Imperative style of programming
+         */
+        List<Integer> uniqueNumbers = new ArrayList<>();
+        for(Integer number:numberList){
+            if(!uniqueNumbers.contains(number)){
+                uniqueNumbers.add(number);
+            }
+        }
+        System.out.println("Imperative style result at the Java: " +uniqueNumbers);
+
+        /**
+         * Declarative style of programming
+         */
+
+        List<Integer> uniqueNumbers1 = numberList
+                .stream()
+                .distinct()
+                .collect(Collectors.toList());
+        System.out.println("Declarative style  result at the Java: " + uniqueNumbers1);
+    }
+}
