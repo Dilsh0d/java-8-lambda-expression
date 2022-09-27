b3225e017eab87e4b57f5bdea8386f173a679c95
Dilshod Tadjiev <dilshod.toj@gmail.com>
2022-09-27 16:43:35 +0500

    Consumer Lesson 17

--- src/uz/dilshod/code/functionalinterface/consumer/ConsumerExample.java
+++ src/uz/dilshod/code/functionalinterface/consumer/ConsumerExample.java
@@ -38,12 +38,12 @@ public class ConsumerExample {
     }
 
     public static void main(String[] args) {
-//        Consumer<String> c1 = (s) -> System.out.println(s.toUpperCase());
-//        c1.accept("java8");
-//
-//        printStudents();
-//
-//        printStudentsWithActivities();
+        Consumer<String> c1 = (s) -> System.out.println(s.toUpperCase());
+        c1.accept("java8");
+
+        printStudents();
+
+        printStudentsWithActivities();
 
         printStudentsWithActivitiesUsingCondition();
     }
