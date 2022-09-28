804693cfd79b2ce1a6a5d0c6867c9488de62cc4a
Dilshod Tadjiev <dilshod.toj@gmail.com>
2022-09-28 17:44:58 +0500

    Predicate Lesson 20

---
+++ src/uz/dilshod/code/functionalinterface/predicate/PredicatePlusConsumerExample.java
@@ -0,0 +1,37 @@
+package uz.dilshod.code.functionalinterface.predicate;
+
+import uz.dilshod.code.functionalinterface.data.Student;
+import uz.dilshod.code.functionalinterface.data.StudentDataBase;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+/**
+ * @author Tadjiev Dilshod
+ * @since 0.1.2
+ */
+public class PredicatePlusConsumerExample {
+
+    Predicate<Student> p1 = (s) -> s.getGradeLevel() >= 3;
+    Predicate<Student> p2 = (s) -> s.getGpa() >= 3.9;
+
+    BiConsumer<String, List<String>> studentBiConsumer = (name, activities) -> System.out.println(name + " : " + activities);
+
+    Consumer<Student> studentConsumer = (student -> {
+        if (p1.and(p2).test(student)) {
+            studentBiConsumer.accept(student.getName(), student.getActivities());
+        }
+    });
+
+    public void printNameAndActivities(List<Student> students) {
+        students.forEach(studentConsumer);
+    }
+
+    public static void main(String[] args) {
+        List<Student> students = StudentDataBase.getAllStudents();
+
+        new PredicatePlusConsumerExample().printNameAndActivities(students);
+    }
+}
