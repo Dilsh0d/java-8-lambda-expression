fd9f3391752852ab2af586e143b03fd75f48d883
Dilshod Tadjiev <dilshod.toj@gmail.com>
2022-12-01 13:42:18 +0500

    Stream api  of(), iterate() and generate() methods

---
+++ src/uz/dilshod/code/streams/factorymethods/StreamOfIterateGenerateExamples.java
@@ -0,0 +1,41 @@
+package uz.dilshod.code.streams.factorymethods;
+
+import java.util.Random;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+/**
+ * @author Tadjiev Dilshod
+ * @since 0.1.2
+ *
+ * Stream API : Factory methods
+ *
+ *  of() - Creates a stream of certain values passed to this method.
+ *      Example:
+ *          Stream<String> stringStream = Stream.of("adam","dan","Julie");
+ *
+ *  generate() - Used to create infinite Streams.
+ *      Example:
+ *          Stream.iterate(1,x->x*2);
+ *
+ *  iterate() - Used to create infinite Streams.
+ *      Example:
+ *          Stream.generate(<Supplier>)
+ */
+public class StreamOfIterateGenerateExamples {
+
+    public static void main(String[] args) {
+        Stream<String> streamOf = Stream.of("Dilshod", "Jahon", "Javoh");
+        streamOf.forEach(System.out::println);
+
+        Stream.iterate(1, x-> x*2)
+                .limit(10)
+                .forEach(System.out::println);
+
+        Supplier<Integer> integerSupplier = new Random()::nextInt;
+        Stream.generate(integerSupplier)
+                .limit(10)
+                .forEach(System.out::println);
+
+    }
+}
